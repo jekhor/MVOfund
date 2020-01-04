a1e68e6fe339b958d448ba40649451f27c9b0dc9
Yauhen Kharuzhy <jekhor@gmail.com>
2020-01-04 21:27:37 +0300

    Upgrade everything

--- Gemfile
+++ Gemfile
@@ -1,28 +1,29 @@
+ruby '2.5.7'
 source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '5.0.0.1'
-gem 'rails-i18n', '5.0.1'
+gem 'rails', '~>5.2.4'
+gem 'rails-i18n'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
+gem 'sass-rails', '~> 5'
 # Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '>= 1.3.0'
+gem 'uglifier'
 # Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.2'
+gem 'coffee-rails'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks', '~> 5'
+gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.5'
+gem 'jbuilder'
 # bundle exec rake doc:rails generates the API under doc/api.
-gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'sdoc', group: :doc
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
@@ -36,24 +37,24 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
-  gem 'rspec-rails', '~> 3.4'
+  gem 'rspec-rails'
   gem 'factory_girl'
 end
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
+  gem 'web-console'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'spring-watcher-listen'
 end
 
 gem 'mysql2'
-gem 'haml-rails', '~> 0.9'
+gem 'haml-rails'
 gem 'responders'
 gem 'simple_form'
-gem 'bootstrap', '~> 4.0.0.alpha3'
+gem 'bootstrap', '~> 4.4.1'
 gem 'tinymce-rails'
 gem 'tinymce-rails-langs'
 gem 'font-awesome-sass'
@@ -63,10 +64,11 @@ gem 'mini_magick'
 gem 'autonumeric-rails'
 gem 'devise'
 gem 'devise-i18n'
-gem 'devise-bootstrap-views', git: 'https://github.com/hisea/devise-bootstrap-views.git', branch: 'bootstrap4'
+gem 'devise-bootstrap-views', '~> 1.0'
 gem "breadcrumbs_on_rails"
 gem 'tether-rails'
 gem 'mini_portile2'
 gem 'meta-tags'
 gem 'rest-client'
+gem 'bootsnap'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,65 +10,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215235726) do
+ActiveRecord::Schema.define(version: 2016_12_15_235726) do
 
   create_table "budget_items", force: :cascade do |t|
-    t.integer  "campaign_id",                                          null: false
-    t.string   "title"
-    t.decimal  "amount",      precision: 12, scale: 2, default: "0.0"
-    t.datetime "created_at",                                           null: false
-    t.datetime "updated_at",                                           null: false
-    t.boolean  "is_expense",                           default: false, null: false
+    t.integer "campaign_id", null: false
+    t.string "title"
+    t.decimal "amount", precision: 12, scale: 2, default: "0.0"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "is_expense", default: false, null: false
     t.index ["campaign_id"], name: "index_budget_items_on_campaign_id"
   end
 
   create_table "campaigns", force: :cascade do |t|
-    t.string   "title",                                                      null: false
-    t.text     "description",                                                null: false
-    t.datetime "created_at",                                                 null: false
-    t.datetime "updated_at",                                                 null: false
-    t.decimal  "target",            precision: 12, scale: 2, default: "0.0"
-    t.date     "end_date"
-    t.text     "short_description",                          default: "",    null: false
-    t.integer  "title_image_id"
+    t.string "title", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.decimal "target", precision: 12, scale: 2, default: "0.0"
+    t.date "end_date"
+    t.text "short_description", default: "", null: false
+    t.integer "title_image_id"
     t.index ["title_image_id"], name: "index_campaigns_on_title_image_id"
   end
 
   create_table "payments", force: :cascade do |t|
     t.datetime "time"
-    t.decimal  "amount",         precision: 12, scale: 2,                 null: false
-    t.string   "contributor"
-    t.text     "notes"
-    t.datetime "created_at",                                              null: false
-    t.datetime "updated_at",                                              null: false
-    t.integer  "campaign_id"
-    t.integer  "budget_item_id"
-    t.boolean  "is_expense",                              default: false, null: false
-    t.string   "payment_number"
+    t.decimal "amount", precision: 12, scale: 2, null: false
+    t.string "contributor"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "campaign_id"
+    t.integer "budget_item_id"
+    t.boolean "is_expense", default: false, null: false
+    t.string "payment_number"
     t.index ["budget_item_id"], name: "index_payments_on_budget_item_id"
   end
 
   create_table "post_images", force: :cascade do |t|
-    t.string   "image_secure_token"
-    t.string   "original_filename"
-    t.string   "image"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.string "image_secure_token"
+    t.string "original_filename"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
