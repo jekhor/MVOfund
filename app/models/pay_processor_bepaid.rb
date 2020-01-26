edfddbbc99cfd54c9f0ccb404cbaea1771695e03
Yauhen Kharuzhy <jekhor@gmail.com>
2020-01-27 00:10:50 +0300

    Switch on test mode for all card payments temporary

--- app/models/pay_processor_bepaid.rb
+++ app/models/pay_processor_bepaid.rb
@@ -12,7 +12,7 @@ class PayProcessorBepaid < PayProcessor
     raise 'PayProcessorBepaid requires :return_url option' if options[:return_url].blank?
     raise 'Description should not be blank' if options[:description].blank?
 
-    test_mode = Rails.env.development?
+    test_mode = true
     return_url = options[:return_url]
 
     checkout_request = {
