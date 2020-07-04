114fe8c67dcca010da751ff39aeea32b90c2dd94
Yauhen Kharuzhy <jekhor@gmail.com>
2020-07-04 19:50:23 +0300

    Set default host for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "mv_ofund_#{Rails.env}"
 
+  config.action_mailer.default_url_options = {host: 'fund.bike.org.by'}
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
